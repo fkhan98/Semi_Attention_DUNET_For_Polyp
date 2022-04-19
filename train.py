34be0ca2947f2224cf918e3c72d63ee18469ad67
unknown <fahimshahriar52@iut-dhaka.edu>
2022-04-19 17:24:15 +0600

    canges in epoch

--- train.py
+++ train.py
@@ -68,7 +68,7 @@ if __name__ == "__main__":
     valid_y = sorted(glob(os.path.join(valid_path, "mask", "*.jpg")))
 
     model_path = "files/model.h5"
-    batch_size = 12
+    batch_size = 8
     epochs = 40
     lr = 1e-5
     shape = (288, 384, 3)
